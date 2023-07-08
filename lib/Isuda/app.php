a888f0dbae3e13b3a631c8990cbc052e44889b6f
工藤颯斗 <kudo@tetemarche.co.jp>
2023-07-08 15:25:58 +0900

    test

--- lib/Isuda/app.php
+++ lib/Isuda/app.php
@@ -52,15 +52,6 @@ $container = new class extends \Slim\Container {
         $pattern = '/(' . implode('|', array_map('preg_quote', $keywordsMap)) . ')/i';
         var_dump($pattern);
 
-        $content = preg_replace_callback($pattern, function ($match) {
-            $keyword = $match[1];
-            $url = '/keyword/' . rawurlencode($keyword);
-            $link = sprintf('<a href="%s">%s</a>', $url, html_escape($keyword));
-            return $link;
-        }, $content);
-
-        echo '<script>console.log('. json_encode($content) .');</script>';
-
         // NOTE: avoid pcre limitation "regular expression is too large at offset"
         for ($i = 0; !empty($kwtmp = array_slice($keywords, 500 * $i, 500)); $i++) {
             $re = implode('|', array_map(function ($keyword) { return quotemeta($keyword['keyword']); }, $kwtmp));
@@ -77,9 +68,6 @@ $container = new class extends \Slim\Container {
 
             $content = preg_replace("/{$hash}/", $link, $content);
         }
-
-        echo '<script>console.log('. json_encode($content) .');</script>';
-        
         return nl2br($content, true);
     }
 
