d1c7965bcab47cf68721dcfa3363a9c6899947d8
工藤颯斗 <kudo@tetemarche.co.jp>
2023-07-08 16:44:23 +0900

    test

--- lib/Isuda/app.php
+++ lib/Isuda/app.php
@@ -170,10 +170,10 @@ $app->post('/keyword', function (Request $req, Response $c) use ($container) {
     }
     $dbh = $container->getDbh();
     $dbh->query(
-        'INSERT INTO entry (keyword, description, created_at, updated_at)'
+        'INSERT INTO entry (author_id, keyword, description, created_at, updated_at)'
         .' VALUES (?, ?, ?, NOW(), NOW())'
         .' ON DUPLICATE KEY UPDATE'
-        .' keyword = ?, description = ?, updated_at = NOW()'
+        .' author_id = ?, keyword = ?, description = ?, updated_at = NOW()'
     , $user_id, $keyword, $description, $user_id, $keyword, $description);
 
     return $c->withRedirect('/');
